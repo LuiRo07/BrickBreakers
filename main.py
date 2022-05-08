a2a0c9a78efc539af20b0ade78c272ea985c4f45
LRosas93 <luisr1993@yahoo.com>
2022-05-07 23:47:31 -0700

    added bricks, ball and brick collision

--- main.py
+++ main.py
@@ -1,93 +1,104 @@
+# TODO: make a list of surfaces (bricks) to update with blits()
+
 import pygame
 import sys
 import os
 
 pygame.init()
 
-# Predefined Colors
-white = (255, 255, 255)
-
-# Main Surface object
-dimension_main = width, height = (800, 600)
-window_surf = pygame.display.set_mode(dimension_main)
 # caption title
-pygame.display.set_caption("Breakers")
+pygame.display.set_caption("Brick Breakers")
 
-# background (surface object)
-bg = pygame.image.load(os.path.join('assets/',
-                                    'background.png'))
-bg = pygame.transform.scale(bg, dimension_main)
-
-# player loading, sizing, and converting to Rect objects
-playerImg = pygame.image.load(os.path.join('assets/',
-                                        'player.png'))
-playerImg = pygame.transform.scale(playerImg, (110,30))
-player_pos = playerX, playerY = 100, 500
-player = playerImg.get_rect(topleft=player_pos)
-
-# Ball loading, sizing, and converting to Rect objects
-ballImg = pygame.image.load(os.path.join('assets/',
-                                         'ball.png'))
-ballImg = pygame.transform.scale(ballImg, (50, 50))
-ball_pos = ballX, ballY = 50, 60
-ball = ballImg.get_rect(topleft=ball_pos)
-ball_speed = [5,5]
-
-# bricks loading, sizing, and converting to Rect objects
-brickImg = pygame.image.load(os.path.join('assets/'
-                                        'brick.png'))
-brickImg = pygame.transform.scale(brickImg, (110,30))
-brick = brickImg.get_rect()
-# dimensions of Rects and its distance from each
-rectwidth = 110
-rectheight = 30
-rectdist = 10
-
-# draw location of where bricks are gonna go, 12 rectangles
-block_position = [] # (left, top) values
-for i in range(6): # columns
-    for j in range(2): # rows
-        x = 20 + i * (rectdist + rectwidth)
-        y = 20 + j * (rectdist + rectheight)
-        block_position.append((x, y))
-
-# creates a list of Rec objects, using elements from block_position
-bricks = [pygame.Rect((x, y), (rectwidth, rectheight)) for x, y in\
-         block_position]
-
+# font to display text
 font = pygame.font.Font("freesansbold.ttf", 64)
+
+# Predefined Colors
+dimgrey = (105, 105, 105)
+white = (255, 250, 250)
 
 # Display Game Over
 def Game_Over():
     over_text = font.render("Game Over", True, (white))
     window_surf.blit(over_text, (200, 150))
     pygame.display.update()
-    pygame.time.wait(5000)
+    pygame.time.wait(4000)
     pygame.quit()
     sys.exit()
 
+def Winner():
+    win_text = font.render("You Win!", True, white)
+    window_surf.blit(win_text, (200, 150))
+    pygame.display.update()
+    pygame.time.wait(4000)
+    pygame.quit()
+    sys.exit()
+
+# creates image surface from asset files
+def create_imgSurf(img_name, size):
+    BASE = 'assets/'
+    surf_img = pygame.image.load(os.path.join(BASE + img_name))
+    surf_img = pygame.transform.scale(surf_img, size)
+    return surf_img
+
+# Main Surface object
+mainSurf_dimensions = surf_width, surf_height = (800, 600)
+window_surf = pygame.display.set_mode(mainSurf_dimensions)
+
+# background (surface object)
+bg = create_imgSurf('background.png', mainSurf_dimensions)
+
+# player
+player_size = (110, 30)
+player_pos = (100, 500)
+playerSurf = create_imgSurf('player.png', player_size)
+playerRect = playerSurf.get_rect(topleft=player_pos)
+
+# Ball
+ball_size = (50, 50)
+ballSurf = create_imgSurf('ball.png', ball_size)
+ball_start = ballX, ballY = (0, 350)
+ballRect = ballSurf.get_rect(topleft=ball_start)
+ball_speeds = {"easy": [2, 2], "medium": [3, 3], "hard": [5, 5]}
+ball_speed = ball_speeds["easy"]
+#----------------------------------------------------------------------------------
+# bricks
+brick_size = (110, 30)
+
+def get_brickPositions():
+    block_position = [] # (left, top) values
+    rectdist = 5
+
+    for i in range(6): # columns
+        for j in range(4): # rows
+            x = 20 + i * (rectdist + brick_size[0])
+            y = 20 + j * (rectdist + brick_size[1])
+            block_position.append((x, y))
+
+    return block_position
+
+brickSurface = create_imgSurf('brick.png', brick_size)
+brick_positions = get_brickPositions()
+brickRecs = [pygame.Rect((x, y), brick_size) for x, y in\
+        brick_positions]
+#--------------------------------------------------------------------------------
+
 # brick and ball collision
-def is_Collision(rec1, rec2, rec_lst):
-    """
-        rec1 param: ball,
-        rec2 param: brick
-        rec_lst param: list of rec objects
-        takes a rec object to tests if it collides with 2nd rect object
-        if so, deletes rec object from list of rects
-        else just returns the list of recs
-    """
-    global ball
-    if rec1.colliderect(rec2):
+def is_Collision():
+    if ballRect.colliderect(brick):
         ball_speed[1] = -(ball_speed[1])
-        ball = ball.move(ball_speed)
-        rec_lst.pop(rec1.collidelist(rec_lst))
+        brickRecs.pop(ballRect.collidelist(brickRecs))
 
-        return rec_lst
-    else:
-        return rec_lst
 
+def check_speed():
+    if len(brickRecs) == len(brickRecs) / 2:
+        ball_speed = ball_speeds["medium"]
+        return ball_speed
+    elif len(brickRecs) == len(brickRecs) * 0.75:
+        ball_speed = ball_speeds["hard"]
+        return ball_speed
+
+# game loop
 while 1:
-
     window_surf.blit(bg, (0,0))
 
     # checks if user quits program with X box
@@ -95,45 +106,40 @@ while 1:
         if event.type == pygame.QUIT:
             sys.exit()
 
-    # ball and player collision
-    if ball.colliderect(player):
-        if ball.collidepoint(player.topleft):
-            ball_speed[1] = -(ball_speed[1])
-            ball = ball.move(ball_speed)
-        elif ball.collidepoint(player.topright):
-            ball_speed[1] = -(ball_speed[1])
-            ball = ball.move(ball_speed)
-        elif ball.collidepoint(player.midtop):
-            ball_speed[1] = -(ball_speed[1])
-            ball = ball.move(ball_speed)
-        elif ball.collidepoint(player.midleft) or ball.collidepoint(player.midright):
-            ball_speed[0] = -(ball_speed[0])
+    # ball boundaries within main surface
+    ballRect = ballRect.move(ball_speed)
 
-    # checking for ball boundary
-    if ball.right >= width or ball.left <= 0:
-        ball_speed[0] = -(ball_speed[0])
-    if ball.bottom >= height or ball.top <= 0:
-        ball_speed[1] = -(ball_speed[1])
-    ball = ball.move(ball_speed)
+    # stores sequence of key pressed
+    keys = pygame.key.get_pressed()
 
     # player movement
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT] and player.right <= width:
-        player = player.move(5,0)
-    if keys[pygame.K_LEFT] and player.left >= 0:
-        player = player.move(-5,0)
+    if keys[pygame.K_RIGHT] and playerRect.right <= surf_width:
+        playerRect = playerRect.move(5, 0)
+    if keys[pygame.K_LEFT] and playerRect.left >= 0:
+        playerRect = playerRect.move(-5,0)
 
-    # checks if all bricks have been cleared, to end game
-    if len(bricks) == 0:
+    # checks for ball boundaries within main window
+    if ballRect.right >= surf_width or ballRect.left <= 0:
+        ball_speed[0] = -(ball_speed[0])
+    if ballRect.top <= 0 or ballRect.bottom >= surf_height:
+        ball_speed[1] = -(ball_speed[1])
+    if ballRect.bottom >= surf_height:
         Game_Over()
 
-    # iterates through list of Rect objects and draws onto surface
-    for brick in bricks:
-        bricks = is_Collision(ball, brick, bricks)
-        window_surf.blit(brickImg, brick)
+    # ball and player collision
+    if ballRect.colliderect(playerRect):
+        ball_speed[1] = -(ball_speed[1])
 
-    window_surf.blit(playerImg, player)
-    window_surf.blit(ballImg, ball)
+    # ball and brick collision
+    for brick in brickRecs:
+        collision = is_Collision()
+        # checks if all bricks have been cleared, to end game
+        if len(brickRecs) == 0:
+            Winner()
+        window_surf.blit(brickSurface, brick)
+
+    window_surf.blit(playerSurf, playerRect)
+    window_surf.blit(ballSurf, ballRect)
     pygame.display.update()
 
 pygame.quit()
